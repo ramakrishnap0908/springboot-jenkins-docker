eb69c47359ee43610fda1075c63ec75cf191b24f
ramakrishnap0908 <ramakrishna0908@Pi-iMac.fios-router.home>
2023-01-30 19:00:16 -0500

    updated ci file

--- src/main/java/com/tech/learning/one/DemoApplication.java
+++ src/main/java/com/tech/learning/one/DemoApplication.java
@@ -3,6 +3,8 @@ package com.tech.learning.one;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class DemoApplication {
 
